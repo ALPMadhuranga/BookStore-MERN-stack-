67fe74a613685ed35516d8cc241165686f157997
Prasad Madhuranga <71040816+ALPMadhuranga@users.noreply.github.com>
2024-02-28 23:43:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}))
 // Option 2: Allow Custom Origins
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://library-management-henna.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
